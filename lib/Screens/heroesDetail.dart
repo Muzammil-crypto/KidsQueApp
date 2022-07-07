9ca7cab03541610a063a714799fbae43285e6f03
Muzammil Rafiq <57823963+Muzammil-crypto@users.noreply.github.com>
2022-07-07 05:22:52 +0500

    re

--- lib/Screens/heroesDetail.dart
+++ lib/Screens/heroesDetail.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:sign_in_interface/Screens/choicesScreen.dart';
 
-class DetailsHeroScreen extends StatelessWidget {
+class DetailsHeroScreen extends StatefulWidget {
   //late String tag;
   late String imgName;
   late String heading;
@@ -22,6 +23,42 @@ class DetailsHeroScreen extends StatelessWidget {
     this.color1 = color1;
     this.color2 = color2;
   }
+
+  @override
+  State<DetailsHeroScreen> createState() => _DetailsHeroScreenState();
+}
+
+class _DetailsHeroScreenState extends State<DetailsHeroScreen> {
+  FlutterTts flutterTts = FlutterTts();
+
+  bool isPlaying = false;
+  speak(String Text) async {
+    await flutterTts.setLanguage("en-US");
+    await flutterTts.setPitch(1);
+
+    if (Text != null && Text.isNotEmpty) {
+      var result = await flutterTts.speak(Text);
+      if (result == 1)
+        setState(() {
+          isPlaying = true;
+        });
+    }
+  }
+
+  Future _stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1)
+      setState(() {
+        isPlaying = false;
+      });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    flutterTts.stop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,51 +67,114 @@ class DetailsHeroScreen extends StatelessWidget {
         child: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
-                colors: [color1, color2],
+                colors: [widget.color1, widget.color2],
                 begin: Alignment.bottomLeft,
                 end: Alignment.topRight),
           ),
-          child: ListView(
+          child: Stack(
             children: [
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height / 1.8,
-                child: Image.network(imgName),
-              ),
-              Column(children: [
-                Container(
-                  child: Container(
-                    child: Container(
-                      margin: EdgeInsets.all(10),
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: [
-                            Text(
-                              heading,
-                              style: TextStyle(
-                                  fontFamily: "BubblegumSans",
-                                  fontSize: 25,
-                                  color: Colors.white),
+              ListView(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height / 1.8,
+                    child: Image.network(widget.imgName),
+                  ),
+                  Stack(children: [
+                    Positioned(
+                      child: Center(
+                        child: Container(
+                          child: Container(
+                            margin: EdgeInsets.all(10),
+                            child: SingleChildScrollView(
+                              child: Column(
+                                children: [
+                                  Text(
+                                    widget.heading,
+                                    style: TextStyle(
+                                        fontFamily: "ShinyballoonDemo",
+                                        fontSize: 25,
+                                        color: Colors.white),
+                                  ),
+                                  Text(
+                                    widget.description,
+                                    style: TextStyle(
+                                        fontFamily: "BubblegumSans",
+                                        fontSize: 16,
+                                        color: Colors.white),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ],
+                              ),
                             ),
-                            Text(
-                              description,
-                              style: TextStyle(
-                                  fontFamily: "BubblegumSans",
-                                  fontSize: 16,
-                                  color: Colors.white),
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
+                          ),
+                          height: MediaQuery.of(context).size.height / 2.2,
+                          width: MediaQuery.of(context).size.width / 1.2,
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20))),
                         ),
                       ),
                     ),
-                    height: MediaQuery.of(context).size.height / 2.2,
-                    width: MediaQuery.of(context).size.width / 1.2,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(20))),
-                  ),
+                    Positioned(
+                      top: 180,
+                      right: 50,
+                      child: Container(
+                          height: 60,
+                          width: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(40),
+                            color: Color.fromARGB(200, 255, 255, 255),
+                          ),
+                          child: FlatButton(
+                              onPressed: () => {
+                                    setState(() {
+                                      isPlaying
+                                          ? _stop()
+                                          : speak(widget.description);
+                                    })
+                                  },
+                              child: isPlaying
+                                  ? Icon(
+                                      Icons.stop,
+                                      color: Colors.red,
+                                      size: 30,
+                                    )
+                                  : Icon(
+                                      Icons.play_arrow,
+                                      color: Colors.green,
+                                      size: 30,
+                                    ))),
+                    ),
+                  ]),
+                ],
+              ),
+              Positioned(
+                top: 50,
+                left: 10,
+                child: Row(
+                  children: [
+                    Container(
+                        height: 35,
+                        width: 35,
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(136, 255, 255, 255),
+                            borderRadius: BorderRadius.circular(7)),
+                        child: Center(
+                          child: IconButton(
+                            icon: Icon(
+                              Icons.arrow_back_ios,
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                            onPressed: () => {
+                              Navigator.pop(context),
+                            },
+                          ),
+                        )),
+                  ],
                 ),
-              ]),
+              ),
             ],
           ),
         ),
--- lib/Screens/historyDetailScreen.dart
+++ lib/Screens/historyDetailScreen.dart
@@ -82,7 +82,7 @@ class _HistoryDetailScreenState extends State<HistoryDetailScreen> {
               child: ListView(
                 children: [
                   Container(
-                    height: MediaQuery.of(context).size.height * 1.9,
+                    height: MediaQuery.of(context).size.height * 1.5,
                     width: MediaQuery.of(context).size.width,
                     decoration: BoxDecoration(color: Colors.cyan),
                     child: Stack(
@@ -166,7 +166,7 @@ class _HistoryDetailScreenState extends State<HistoryDetailScreen> {
                           ),
                         ),
                         Positioned(
-                          bottom: MediaQuery.of(context).size.height / 2 + 440,
+                          bottom: MediaQuery.of(context).size.height / 2 + 125,
                           right: 40,
                           child: Container(
                               height: 60,
@@ -220,74 +220,6 @@ class _HistoryDetailScreenState extends State<HistoryDetailScreen> {
                           ),
                         ),
                         Positioned(
-                            top: MediaQuery.of(context).size.height / 0.72,
-                            child: Container(
-                                child: Text(
-                              "Related Places",
-                              style: TextStyle(
-                                  fontFamily: "ShinyBalloonDemo",
-                                  fontSize: 40,
-                                  color: Colors.white),
-                            ))),
-                        Positioned(
-                          top: MediaQuery.of(context).size.height / 0.67,
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                              left: 20.0,
-                            ),
-                            child: SizedBox(
-                              height: 120,
-                              width: MediaQuery.of(context).size.width,
-                              child: ListView(
-                                shrinkWrap: true,
-                                scrollDirection: Axis.horizontal,
-                                children: <Widget>[
-                                  Container(
-                                    margin: EdgeInsets.only(right: 5),
-                                    height: 120,
-                                    width: 120.0,
-                                    child: CircleAvatar(
-                                        backgroundImage: NetworkImage(
-                                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHBVFSOJ6Ll438ccvbWTFPFBkDeITdUfoPOA&usqp=CAU")),
-                                  ),
-                                  Container(
-                                    margin: EdgeInsets.only(right: 5),
-                                    height: 120,
-                                    width: 120.0,
-                                    child: CircleAvatar(
-                                        backgroundImage: NetworkImage(
-                                            "https://www.explorerpakistan.com/wp-content/uploads/2019/09/1-lahore-fort-800x445.jpg")),
-                                  ),
-                                  Container(
-                                    margin: EdgeInsets.only(right: 5),
-                                    height: 120,
-                                    width: 120.0,
-                                    child: CircleAvatar(
-                                        backgroundImage: NetworkImage(
-                                            "https://media.istockphoto.com/photos/wazir-khan-mosque-lahore-pakistan-picture-id1130761760?k=20&m=1130761760&s=612x612&w=0&h=OPX8vVKJnwaAASLUO23nVm0ysAp8k5mugGfbu0XuJyg=")),
-                                  ),
-                                  Container(
-                                    margin: EdgeInsets.only(right: 5),
-                                    height: 120,
-                                    width: 120.0,
-                                    child: CircleAvatar(
-                                        backgroundImage: NetworkImage(
-                                            "https://pbs.twimg.com/media/DN0CNlDWkAIbhBf.jpg")),
-                                  ),
-                                  Container(
-                                    margin: EdgeInsets.only(right: 5),
-                                    height: 120,
-                                    width: 120.0,
-                                    child: CircleAvatar(
-                                        backgroundImage: NetworkImage(
-                                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHBVFSOJ6Ll438ccvbWTFPFBkDeITdUfoPOA&usqp=CAU")),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                        Positioned(
                           top: 20,
                           left: 10,
                           child: Row(
@@ -314,7 +246,7 @@ class _HistoryDetailScreenState extends State<HistoryDetailScreen> {
                           ),
                         ),
                         Positioned(
-                          bottom: MediaQuery.of(context).size.height / 8,
+                          bottom: MediaQuery.of(context).size.height / 15,
                           child: SingleChildScrollView(
                             child: RaisedButton(
                               onPressed: () {
