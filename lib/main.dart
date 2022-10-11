d6a5670b7d710a725349a9cad7abd9a3dba8bea1
Muzammil Rafiq <57823963+Muzammil-crypto@users.noreply.github.com>
2022-10-12 02:34:49 +0500

    main modified

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: "/",
       onGenerateRoute: RouteGenerator.generateRoute,
-      //home: SplashScreen(),
       home: SplashScreen(),
     );
   }
